b8f8a82c556c8261ef6dd4b9a0cc90a624fdd7e4
hundredplans <hundredplans@gmail.com>
2023-09-03 23:45:37 +0100

    new

--- fof/scenes/ui_general/scale_button/scale_button.gd
+++ fof/scenes/ui_general/scale_button/scale_button.gd
@@ -11,6 +11,10 @@ func _ready() -> void:
 	if default >= totalmin and default <= totalmax:
 		$Number.text = str(default)
 	
-	$Outside.color = Helper.DARK_BROWN
-	$Inside.color = Helper.LIGHT_BROWN
 	$Label.text = label_text
+	
+	for child in [$Outside, $Steppers/Left/BigStep, $Steppers/Right/BigStep]:
+		child.color = Helper.DARK_BROWN
+	
+	for child in [$Inside, $Steppers/Left/SmallStep, $Steppers/Right/SmallStep]:
+		child.color = Helper.LIGHT_BROWN
