5b64967d2e4c717ca474e2fcf130ffe6539d4ede
hundredplans <hundredplans@gmail.com>
2023-09-23 13:57:58 +0100

    new

--- fof/scenes/editor/file_loader/file_loader.gd
+++ fof/scenes/editor/file_loader/file_loader.gd
@@ -1,5 +1,8 @@
 extends Control
-signal load_info
+
+const static_path: String = "res://static/base_game/"
+var items: Array
+var item_name: String
 var base_path: String
 var current_path: String
 
@@ -7,8 +10,10 @@ func _ready() -> void:
 	$LoadInOut.play_backwards("load_in_out")
 	$ExitButton.pressed.connect(func(): Helper.play_method_on_animation_end("load_in_out", $LoadInOut, queue_free, [], false, self))
 
-func on_ready(file_loader_name: String) -> void:
-	var path: String = file_loader_name.to_lower() + "s/"
-	base_path = path
-	current_path = path
-
+func on_ready(_item_name: String) -> void:
+	item_name = _item_name.to_lower()
+	var path: String = item_name + "s/"
+	base_path = static_path + path
+	current_path = static_path + path
+	items = Helper.return_file_names_recursive(base_path.left(-1)).map(func(x: String): Helper.return_file_contents(x))
+	print(items)
