582179c88e9af4b6dfc0707ecb52c1ecc194647d
hundredplans <hundredplans@gmail.com>
2024-01-05 15:36:49 +0000

    sand walk added

--- fof/scenes/autoload/audio_master.gd
+++ fof/scenes/autoload/audio_master.gd
@@ -1,9 +1,6 @@
 extends Node
 @onready var music_stream: AudioStreamPlayer = $MusicStream
 @onready var sfx_container: Node = $SFXContainer
-
-var loop_sfx: Array = ["sand_walk"]
-
 @onready var sfx_library: Dictionary = {
 	"confirm_default": [preload("res://assets/sounds/confirmation/confirm_default.wav"), -10],
 	"unconfirm_default": [preload("res://assets/sounds/confirmation/unconfirm_default.wav"), -10],
@@ -15,12 +12,6 @@ var loop_sfx: Array = ["sand_walk"]
 	"woosh": [preload("res://scenes/ui_general/arrow/woosh.wav"), 0],
 	"sand_walk": [preload("res://assets/sounds/walk/sand_walk.wav"), 0]
 }
-
-func _ready() -> void:
-	for sfx in sfx_library:
-		if sfx in loop_sfx:
-			sfx_library[sfx][0].loop_mode = AudioStreamWAV.LOOP_FORWARD
-			sfx_library[sfx][0].loop_end = sfx_library[sfx][0].data.size()
 
 func play_sfx(sfx: String, volume_offset: int = 0, early_cutoff: float = 0) -> void:
 	for child in sfx_container.get_children():
