a2e6f555c1653147e891028e06ef68fd6d5b9809
hundredplans <hundredplans@gmail.com>
2024-06-15 21:37:04 +0200

    cof

--- cof/main/class_select/class_select.php
+++ cof/main/class_select/class_select.php
@@ -1,26 +1,110 @@
 <?php
+include_once('../helper.php');
+$mysqli = onAccessDatabase();
+onCreateTables($mysqli);
 
-function onCreateClassButtons() {
-   $class_info = file_get_contents('../../static/classes/class_info.json');
-   $class_info = json_decode($class_info, true);
-   $class_info = $class_info["class_info"];
-   foreach ($class_info as $info) {
-       $name = $info["name"];
-       echo "<div> <button type='submit' name='class_select'> $name </button></div>";
-   }
+$class_select = isset($_GET['class_select']);
+$remove_class = isset($_GET['remove_class']);
+$main_menu = isset($_GET['main_menu']);
+if ($class_select) {onClassSelected($mysqli);}
+if ($remove_class) {onClassRemoved($mysqli);}
+if ($class_select or $remove_class) {
+    header("Location: http://$_SERVER[HTTP_HOST]/cof/main/class_select/class_select.php");
 }
-function onCreateSelectedClasses() {
+elseif ($main_menu) {
+    header("Location: http://$_SERVER[HTTP_HOST]/cof/main/main_menu/main_menu.php");
+}
+function onCreateTables($mysqli) {
+    $create_saves_query = "CREATE TABLE IF NOT EXISTS Saves(
+        save_id INT NOT NULL AUTO_INCREMENT,
+        level_id INT NOT NULL DEFAULT '1',
+        in_map INT NOT NULL DEFAULT '0',
+        PRIMARY KEY (save_id))";
+    # in_map oscillates between 0 and 1 depending on if you're in level or not
 
+    $create_saves_classes_query = "CREATE TABLE IF NOT EXISTS SavesClasses(
+    save_id INT NOT NULL,
+    class_id INT NOT NULL,
+    PRIMARY KEY (save_id, class_id))";
+
+    $mysqli -> query($create_saves_query);
+    $mysqli -> query($create_saves_classes_query);
+}
+function onAccessDatabase(): mysqli
+{
+    //mysqli_report(MYSQLI_REPORT_OFF);
+    $dbhost = 'localhost';
+    $dbuser = 'witold';
+    $dbpass = 'witold';
+    $dbname = 's32007';
+    return new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+}
+function onCreateClassButtons($mysqli): void {
+    $disabled_classes = getSelectedClasses($mysqli);
+    $size = sizeof($disabled_classes);
+    foreach ($GLOBALS['classes_info'] as $info) {
+        $name = $info["name"];
+        $id = $info["id"];
+
+        $is_disabled = (in_array($id, $disabled_classes) or $size == 4);
+        if ($is_disabled) {$is_disabled = 'class=btn_disabled disabled';}
+        else {$is_disabled = 'class=btn_regular';}
+
+        echo "<div> <button type='submit' name='class_select' value='$id' $is_disabled>
+        $name</button> <img src='../../static/class_sprites/front/$id.png' alt='Front Sprite'></div>";
+       }
+}
+
+function onClassSelected($mysqli) {
+    print_r(sizeof(getSelectedClasses($mysqli)));
+    if (sizeof(getSelectedClasses($mysqli)) < $GLOBALS['MAX_CLASS_COUNT']){
+        $class_id = $_GET['class_select'];
+        $insert_query = "INSERT INTO SavesClasses(class_id, save_id) VALUES ('$class_id', 1)";
+        $mysqli->query($insert_query);
+    }
+}
+
+function onCreateSelectedClasses($mysqli) {
+    $class_ids = getSelectedClasses($mysqli);
+
+    if (sizeof($class_ids) > 0) {
+        echo "<div id='selected_classes'>";
+    }
+    foreach ($class_ids as $id) {
+        $name = getClassInfo($id)['name'];
+        echo "<div><button type='submit' name='remove_class' value='$id' class='large_button'> $name </button></div>";
+    }
+    echo "</div>";
+    if (sizeof($class_ids) == 4) {
+        echo "<button type='submit' class='large_button'> Play </button>";
+    }
+}
+
+function onClassRemoved($mysqli) {
+    $class_id = $_GET['remove_class'];
+    $delete_query = "DELETE FROM SavesClasses WHERE class_id = '$class_id' AND save_id = '1'";
+    $mysqli->query($delete_query);
+}
+
+function onCreateClassesHeader($mysqli) {
+   $classes = getSelectedClasses($mysqli);
+   if (sizeof($classes) > 0) {echo '<h1> Press below to remove a class (MAX = 4) </h1>';}
+   else {echo '<h1> Press above to add a class (MAX = 4) </h1>';}
 }
 ?>
 <link href="class_select.css" rel="stylesheet">
 <html lang="en">
 <body>
-<div id="classes">
-    <?php
-    onCreateClassButtons();
-    onCreateSelectedClasses();
-    ?>
-</div>
+<form action="class_select.php">
+    <div id="classes">
+    <?php onCreateClassButtons($mysqli); ?>
+    </div>
+
+    <?php onCreateClassesHeader($mysqli) ?>
+    <div id="bottom_side">
+        <button type="submit" class="large_button" name="main_menu"> Main Menu </button>
+        <?php onCreateSelectedClasses($mysqli); ?>
+    </div>
+</form>
 </body>
 </html>
---
+++ cof/main/helper.php
@@ -0,0 +1,27 @@
+<?php
+$MAX_CLASS_COUNT = 4;
+
+$classes_info = file_get_contents('../../static/classes/classes_info.json');
+$classes_info = json_decode($classes_info, true);
+$classes_info = $classes_info["classes_info"];
+
+function getSelectedClasses($mysqli): array {
+    $select_query = "SELECT class_id FROM SavesClasses WHERE save_id = '1'";
+    $result = $mysqli -> query($select_query);
+    $class_ids = array();
+    if ($result -> num_rows > 0) {
+        while ($row = $result -> fetch_assoc()) {
+            array_push($class_ids, $row["class_id"]);
+        }
+    }
+    return $class_ids;
+}
+
+function getClassInfo($id): array {
+    foreach ($GLOBALS['classes_info'] as $info) {
+       if ($info['id'] == $id) {
+           return $info;
+       }
+    }
+    return array();
+}
